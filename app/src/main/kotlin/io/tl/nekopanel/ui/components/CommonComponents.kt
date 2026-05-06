7fb3b066093d9d6605a9828ff108404d8eb58ee1
TanakaLun <2709410138@qq.com>
2026-05-06 23:10:59 +0800

    fix:opt: tabrow

--- app/src/main/kotlin/io/tl/nekopanel/ui/components/CommonComponents.kt
+++ app/src/main/kotlin/io/tl/nekopanel/ui/components/CommonComponents.kt
@@ -50,6 +50,7 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
