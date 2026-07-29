a170b8eff24e20e120d27645d8ae470d2e68acea
TanakaLun <2709410138@qq.com>
2026-07-29 08:24:16 +0800

    fix: Modifier.matchParentSize() with correct prefix

--- app/src/main/kotlin/io/tl/nekopanel/MainActivity.kt
+++ app/src/main/kotlin/io/tl/nekopanel/MainActivity.kt
@@ -423,7 +423,7 @@ internal fun MainScreenContent(
                     ) {
                         if (showBlur) {
                             Box(
-                                modifier = matchParentSize()
+                                modifier = Modifier.matchParentSize()
                                     .graphicsLayer {
                                         alpha = scrollBehavior.state
                                             ?.let { (-it.contentOffset / 48.dp.toPx()).coerceIn(0f, 1f) }
