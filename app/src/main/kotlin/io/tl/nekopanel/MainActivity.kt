1f7a2c9946eb1761635c21f65870578b0a9860fd
TanakaLun <2709410138@qq.com>
2026-07-31 01:28:25 +0800

    fix: add missing graphicsLayer import

--- app/src/main/kotlin/io/tl/nekopanel/MainActivity.kt
+++ app/src/main/kotlin/io/tl/nekopanel/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
