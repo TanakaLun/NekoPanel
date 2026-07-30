f6610377afc47797f733cdb95c5df55883ff2343
TanakaLun <2709410138@qq.com>
2026-07-31 04:30:04 +0800

    fix: restore connection detail scroll state

--- app/src/main/kotlin/io/tl/nekopanel/ui/screens/ConnectionsView.kt
+++ app/src/main/kotlin/io/tl/nekopanel/ui/screens/ConnectionsView.kt
@@ -3,6 +3,7 @@ package io.tl.nekopanel.ui.screens
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
